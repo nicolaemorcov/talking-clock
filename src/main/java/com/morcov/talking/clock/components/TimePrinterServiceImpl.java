8da6ee70cf5af7aa94c25b7f8caabd50f1f8438a
Nicol <morkovnicolae@gmail.com>
2022-03-08 21:11:45 +0000

    fixed midnight time

--- src/main/java/com/morcov/talking/clock/components/TimePrinterServiceImpl.java
+++ src/main/java/com/morcov/talking/clock/components/TimePrinterServiceImpl.java
@@ -18,36 +18,28 @@ public class TimePrinterServiceImpl implements TimePrinterService{
     @Override
     public String printTime(int hour, int minutes){
         System.out.println("\n\n"); //this will separate the time from the rest of the logs
+        String midnight = "midnight";
+        String hourString = ((hour == 0) ? "midnight" : names[hour]);
         if (minutes == 0){
             if (hour == 0){
-                return "It's midnight 00:00";
+                return "It's " + midnight + " 00:00";
             }else {
-                return names[hour] + "o'clock";
-
-//                System.out.printf("%s o'clock\n", names[hour]);
+                return hourString + "o'clock";
             }
         }else if (minutes == 1){
-            return "One minute past " + names[hour];
-//            System.out.printf("One minute past %s\n", names[hour]);
+            return "One minute past " + hourString;
         }else if (minutes == 59){
-            return "One minute to " + names[hour];
-//            System.out.printf("One minute to %s\n", names[hour]);
+            return "One minute to " + hourString;
         }else if (minutes == 15){
-            return "Quarter past " + names[hour];
-//            System.out.printf("Quarter past %s\n", names[hour]);
+            return "Quarter past " + hourString;
         }else if (minutes == 30){
-            return "Half past " + names[hour];
-//            System.out.printf("Half past %s\n", names[hour]);
+            return "Half past " + hourString;
         }else if (minutes == 45){
-            return "Quarter to " + names[hour];
-//            System.out.printf("Quarter to %s\n", names[hour]);
+            return "Quarter to " + hourString;
         }else if (minutes <= 30) {
-            return names[minutes] + " minutes past " + names[hour];
-//            System.out.printf("%s minutes past %s\n", names[minutes], names[hour]);
+            return names[minutes] + " minutes past " + hourString;
         } else {
             return names[60 - minutes] + " minutes to " + names[(hour % 12) + 1];
-//            System.out.printf("%s minutes to %s\n", names[60 - minutes],
-//                    names[(hour % 12) + 1]);
         }
     }
 
